7002a1bf7b64aef1d7d9ee7e5b7b3a82974474d9
QuangNM <quangnm910@gmail.com>
2023-02-08 19:26:46 +0700

    create category details screen

---
+++ lib/presentation/scenes/category_details/category_details_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../base/product_cart.dart';
+
+class CategoryDetails extends StatelessWidget {
+  const CategoryDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        title: const Text('Clothes'),
+        centerTitle: true,
+        actions: const [
+          Padding(
+              padding: EdgeInsets.only(right: 16), child: Icon(Icons.search))
+        ],
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.6,
+            crossAxisSpacing: 24,
+            mainAxisSpacing: 24),
+        shrinkWrap: true, // You won't see infinite size error
+        itemCount: 5,
+        itemBuilder: (BuildContext ctx, index) {
+          return const ProductCard();
+        },
+      ),
+    );
+  }
+}
